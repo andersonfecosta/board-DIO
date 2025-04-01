54d50e902af400988148110be432e06eb30dbf8f
Anderson Costa <andersonfelipedc@gmail.com>
2025-04-01 04:24:48 -0300

    BoardColumnDAO, BoardQueryService

--- src/main/java/service/BoardService.java
+++ src/main/java/service/BoardService.java
@@ -1,7 +1,9 @@
 package service;
 
 import lombok.AllArgsConstructor;
+import persistence.dao.BoardColumnDAO;
 import persistence.dao.BoardDAO;
+import persistence.entity.BoardEntity;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,6 +12,26 @@ import java.sql.SQLException;
 public class BoardService {
 
     private final Connection connection;
+
+    public BoardEntity insert(final BoardEntity entity) throws SQLException{
+        var dao = new BoardDAO(connection);
+        var boardColumnDao = new BoardColumnDAO(connection);
+        try {
+            dao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+                c.setBoard(entity);
+                return c;
+            }).toList();
+            for (var column : columns) {
+                boardColumnDao.insert(column);
+            }
+            connection.commit();
+        }catch (SQLException e) {
+            connection.rollback();
+            throw e;
+        }
+        return entity;
+    }
 
     public boolean delete(final Long id) throws SQLException {
         var dao = new BoardDAO(connection);
